65d3bdd52232c5d8cdd83cdf3b27f0f5cd9f823c
Colin Spangelo <colin.spangelo@gmail.com>
2023-07-10 16:37:57 -0600

    Checked curl command line arguments

--- express_server.js
+++ express_server.js
@@ -6,9 +6,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//Says hello to the client
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+//will send the ulrDatabase object to the client
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+//Can use HTML
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
